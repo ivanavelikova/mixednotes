c9491836bb744b73f4591a243d925ac7740bda43
Ivana Velikova <ivanavelikova@abv.bg>
2019-04-01 13:31:36 +0300

    Remove views code

--- mixednotes/MixedNotes/Program.cs
+++ mixednotes/MixedNotes/Program.cs
@@ -8,61 +8,11 @@ using MixedNotes.Views;
 
 namespace MixedNotes
 {
-    public static class Program
+    static class Program
     {
-        public static NoteController noteController = new NoteController(new mixednotesdbEntities());
-        public static ListController listController = new ListController(new mixednotesdbEntities());
-        public static TaskController taskController = new TaskController(new mixednotesdbEntities());
-
         static void Main(string[] args)
         {
-            WindowSettings();
-            StartMenu();
-        }
 
-        /// <summary>
-        /// Changes window's colors
-        /// </summary>
-        private static void WindowSettings()
-        {
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.Clear();
-
-            Console.Title = "MixedNotes.exe";
-        }
-
-        public static void StartMenu()
-        {
-            Console.WriteLine("Hello!");
-            Console.WriteLine("What would you like to do today?");
-            Console.WriteLine("1. Open notes");
-            Console.WriteLine("2. Open TODO lists");
-            Console.WriteLine("3. Exit");
-
-            int inputCommand = int.Parse(Console.ReadLine());
-
-            switch (inputCommand)
-            {
-                case 1:
-                    OpenNotes();
-                    break;
-
-                case 2:
-                    OpenTODOLists();
-                    break;
-
-                case 3:
-                    Exit();
-                    break;
-            }
-        }
-
-        public static void Exit()
-        {
-            Console.Clear();
-            Console.WriteLine("Have a good day!");
-            Environment.Exit(0);
         }
     }
 }
--- mixednotes/MixedNotes/Views/NoteView.cs
+++ mixednotes/MixedNotes/Views/NoteView.cs
@@ -8,18 +8,5 @@ namespace MixedNotes.Views
 {
     public class NoteView
     {
-        public static void OpenNotes()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Notes");
-            Console.WriteLine("1. View all notes");
-            Console.WriteLine("2. Create new note");
-            Console.WriteLine("3. Go back");
-
-            int inputCommand = int.Parse(Console.ReadLine());
-
-
-        }
     }
 }
