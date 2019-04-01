6f5fa9bd8d486b88aecf514acf5e48f58ad156bf
TeodoraDroseva <x_anon_y_mous@gmail.com>
2019-04-01 11:01:42 +0300

    Add main menu view

--- mixednotes/MixedNotes/Program.cs
+++ mixednotes/MixedNotes/Program.cs
@@ -3,25 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MixedNotes.Controllers;
+using MixedNotes.Views;
 
 namespace MixedNotes
 {
-    class Program
+    public static class Program
     {
+        public static NoteController noteController = new NoteController(new mixednotesdbEntities());
+        public static ListController listController = new ListController(new mixednotesdbEntities());
+        public static TaskController taskController = new TaskController(new mixednotesdbEntities());
+
         static void Main(string[] args)
         {
-            using (var db = new mixednotesdbEntities())
+            WindowSettings();
+            StartMenu();
+        }
+
+        /// <summary>
+        /// Changes window's colors
+        /// </summary>
+        private static void WindowSettings()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.Title = "MixedNotes.exe";
+        }
+
+        public static void StartMenu()
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What would you like to do today?");
+            Console.WriteLine("1. Open notes");
+            Console.WriteLine("2. Open TODO lists");
+            Console.WriteLine("3. Exit");
+
+            int inputCommand = int.Parse(Console.ReadLine());
+
+            switch (inputCommand)
             {
-                var notes = db.notes.ToList();
+                case 1:
+                    OpenNotes();
+                    break;
 
-                note test = new note();
+                case 2:
+                    OpenTODOLists();
+                    break;
 
-                test.content = "TestNoteyadayada";
-
-                db.notes.Add(test);
-
-                db.SaveChanges();
+                case 3:
+                    Exit();
+                    break;
             }
+        }
+
+        public static void Exit()
+        {
+            Console.Clear();
+            Console.WriteLine("Have a good day!");
+            Environment.Exit(0);
         }
     }
 }
---
+++ mixednotes/MixedNotes/Views/NoteView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedNotes.Views
+{
+    public class NoteView
+    {
+        public static void OpenNotes()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Notes");
+            Console.WriteLine("1. View all notes");
+            Console.WriteLine("2. Create new note");
+            Console.WriteLine("3. Go back");
+
+            int inputCommand = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
