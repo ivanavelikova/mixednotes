c6ef279cc37c680d0d4e5b7424d32a198a4984af
Ivana Velikova <ivanavelikova@abv.bg>
2019-04-01 09:59:08 +0300

    Add controllers

---
+++ mixednotes/MixedNotes/Controllers/ListController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace MixedNotes.Controllers
+{
+    public class ListController
+    {
+        public mixednotesdbEntities MixedNotesDbEntities { get; }
+
+        public ListController(mixednotesdbEntities mixedNotesDbEntities)
+        {
+            MixedNotesDbEntities = mixedNotesDbEntities;
+        }
+
+        /// <summary>
+        /// Adds the list in the db
+        /// </summary>
+        /// <param name="list"></param>
+        public void AddList(list list)
+        {
+            MixedNotesDbEntities.lists.Add(list);
+            MixedNotesDbEntities.SaveChanges();
+        }
+
+        /// <summary>
+        /// Edits the list's content
+        /// </summary>
+        public void EditList(list changedList)
+        {
+            list list = MixedNotesDbEntities.lists.Find(changedList.list_id);
+
+            MixedNotesDbEntities.Entry(list).CurrentValues.SetValues(changedList);
+            MixedNotesDbEntities.SaveChanges();
+        }
+
+        /// <summary>
+        /// Removes the list from the db
+        /// </summary>
+        /// <param name="list"></param>
+        public void RemoveList(list list)
+        {
+            MixedNotesDbEntities.lists.Remove(list);
+            MixedNotesDbEntities.SaveChanges();
+        }
+
+        /// <summary>
+        /// Lists all lists from the db
+        /// </summary>
+        /// <returns></returns>
+        public List<list> GetAllLists()
+        {
+            return MixedNotesDbEntities.lists.ToList();
+        }
+    }
+}
---
+++ mixednotes/MixedNotes/Controllers/NoteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace MixedNotes.Controllers
+{
+    public class NoteController
+    {
+        public mixednotesdbEntities MixedNotesDbEntities { get; }
+
+        public NoteController(mixednotesdbEntities mixedNotesDbEntities)
+        {
+            MixedNotesDbEntities = mixedNotesDbEntities;
+        }
+
+        /// <summary>
+        /// Adds the note in the db
+        /// </summary>
+        /// <param name="note"></param>
+        public void AddNote(note note)
+        {
+            MixedNotesDbEntities.notes.Add(note);
+            MixedNotesDbEntities.SaveChanges();
+        }
+
+        /// <summary>
+        /// Edits the note's content
+        /// </summary>
+        public void EditNote(note changedNote)
+        {
+            note note = MixedNotesDbEntities.notes.Find(changedNote.note_id);
+
+            MixedNotesDbEntities.Entry(note).CurrentValues.SetValues(changedNote);
+            MixedNotesDbEntities.SaveChanges();
+        }
+
+        /// <summary>
+        /// Removes the note from the db
+        /// </summary>
+        /// <param name="note"></param>
+        public void RemoveNote(note note)
+        {
+            MixedNotesDbEntities.notes.Remove(note);
+            MixedNotesDbEntities.SaveChanges();
+        }
+
+        /// <summary>
+        /// Lists all notes from the db
+        /// </summary>
+        /// <returns></returns>
+        public List<note> GetAllNotes()
+        {
+            return MixedNotesDbEntities.notes.ToList();
+        }
+    }
+}
